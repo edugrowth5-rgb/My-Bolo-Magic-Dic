a0e691a5c53ccef358ecf74aca07960c2d8e57c4
edugrowth5-rgb <edugrowth5@gmail.com>
2026-02-27 18:07:15 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'magic-v1';
+const assets = ['./', './index.html', './app.js', './manifest.json', './favicon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
